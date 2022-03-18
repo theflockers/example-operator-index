f3735c0d0498cb89d7f9cca84b97e92c2b57a99e
Jordan <jordan@nimblewidget.com>
2022-03-18 15:26:32 -0500

    filter out document separators from input

--- convert.sh
+++ convert.sh
@@ -5,5 +5,5 @@ out=$(cat $1)
 while IFS= read -r img; do
 	export rendered=$(bin/opm render "$img" -o yaml)
 	out=$(echo "$out" | ${YQ} "(select(.schema == \"olm.bundle\" and .image == \"$img\")) |= env(rendered)" -)
-done <<< $(echo "$out" | ${YQ} "select(.schema == \"olm.bundle\") | [.image][]" -)
+done <<< $(echo "$out" | ${YQ} "select(.schema == \"olm.bundle\") | [.image][]" - | sed -e '/^---[ 	]*$/d')
 echo "$out"
