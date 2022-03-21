b17dc3bc02f8e479f8ec8779060917ef12ca5066
Jordan <jordan@nimblewidget.com>
2022-03-21 09:22:08 -0500

    restoring eval-all

--- convert.sh
+++ convert.sh
@@ -4,6 +4,6 @@ set -eu -o pipefail
 out=$(cat $1)
 while IFS= read -r img; do
 	export rendered=$(bin/opm render "$img" -o yaml)
-	out=$(echo "$out" | ${YQ} "(select(.schema == \"olm.bundle\" and .image == \"$img\")) |= env(rendered)" -)
-done <<< $(echo "$out" | ${YQ} "select(.schema == \"olm.bundle\") | [.image][]" - | sed -e '/^---[ 	]*$/d')
+	out=$(echo "$out" | ${YQ} eval-all "(select(.schema == \"olm.bundle\" and .image == \"$img\")) |= env(rendered)" -)
+done <<< $(echo "$out" | ${YQ} eval-all "select(.schema == \"olm.bundle\") | [.image][]" -)
 echo "$out"
